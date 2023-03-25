3189908a645725a0dcc1ad2d053fdacda86522d6
Somnath Dey <sdey.aklakhi@gmail.com>
2023-03-25 15:23:32 +0530

    created most of components, done navbar and hero section styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Categories from "./pages/categories/Categories";
+import Home from "./pages/home/Home";
+import ProductDetail from "./pages/productDetail/ProductDetail";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Categories />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
