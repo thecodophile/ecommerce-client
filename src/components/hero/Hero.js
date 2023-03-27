a7f975d1a32505acc22b86b09a6fb919c946601e
Somnath Dey <sdey.aklakhi@gmail.com>
2023-03-27 10:08:31 +0530

    created categories page

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Hero.scss";
 
 const Hero = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="Hero">
       <div className="hero-content center">
@@ -9,7 +12,12 @@ const Hero = () => {
         <p className="subheading">
           Exclusive Art Pieces, for the Exclusive You.
         </p>
-        <button className="cta btn-primary">Explore more</button>
+        <button
+          onClick={() => navigate("/category")}
+          className="cta btn-primary"
+        >
+          Explore more
+        </button>
       </div>
     </div>
   );
